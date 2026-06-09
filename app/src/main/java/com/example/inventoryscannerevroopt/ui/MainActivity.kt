5fd052eb4a58a6cbbe669159786baee56eefe249
vider <dvdblr@mail.ru>
2026-06-09 10:22:59 +0300

    Настроил зависимости и версии, код запускается на устройстве

--- app/src/main/java/com/example/inventoryscannerevroopt/ui/MainActivity.kt
+++ app/src/main/java/com/example/inventoryscannerevroopt/ui/MainActivity.kt
@@ -1,2 +1,14 @@
 package com.example.inventoryscannerevroopt.ui
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.inventoryscannerevroopt.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
